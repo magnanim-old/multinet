27cd1271f73856ac15a19bd1a72b1494de097f7a
Mikael Dubik <mkdubik@gmail.com>
2017-02-07 11:49:25 +0100

    Remove config files and prepare signature

---
+++ multinet/include/community/flattening.h
@@ -0,0 +1,13 @@
+#ifndef FLATTENING_H_
+#define FLATTENING_H_
+
+#include "datastructures.h"
+
+namespace mlnet {
+
+class berlingerio{};
+class rocklin_and_pinar{};
+class bonchi{};
+
+#endif
+
---
+++ multinet/include/community/lbl.h
@@ -0,0 +1,13 @@
+#ifndef LBL_H_
+#define LBL_H_
+
+#include "datastructures.h"
+
+namespace mlnet {
+
+class mimag {};
+class abacus {};
+class cpm {};
+class pmm {};
+
+#endif
