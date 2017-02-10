d2db9c061d98d33c7b87f9d5a95bfa63773fdc0a
Mikael Dubik <mkdubik@gmail.com>
2017-02-10 14:21:45 +0100

    Preparing for the first test

---
+++ multinet/src/community/multilayer/lart.cpp
@@ -0,0 +1,14 @@
+#include "datastructures.h"
+#include "community/multilayer.h"
+
+
+namespace mlnet {
+
+
+actor_list lart::get_ml_community(MLNetworkSharedPtr mnet, uint32_t t, float eps, float gamma) {
+
+	return NULL;
+
+}
+
+}
